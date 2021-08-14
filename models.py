99a735bd391548ec6681d20d098ea574da240839
Faisal mac <faisal12499@hotmail.com>
2021-08-14 03:56:18 +0300

    deployment

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ load_dotenv()
 db_UserAndPass = os.getenv('DB_USER_AND_PASS', 'postgres:postgres')
 db_host = os.getenv('DB_HOST', 'localhost:5432')
 db_name = os.getenv('DB_NAME', "capstone")
-db_path = os.getenv("DATABASE_URL", "postgresql://{}@{}/{}".format(
+db_path = os.getenv("DATBASE_URL", "postgresql://{}@{}/{}".format(
     db_UserAndPass, db_host, db_name))
 # db_path = "postgresql://{}@{}/{}".format(
 #     db_UserAndPass, db_host, db_name)
